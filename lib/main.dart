4edbd1c077a5421edda2720dd8d86a75f235a67a
Bill Sanderson <bill@towniebagels.com>
2025-08-23 08:16:29 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:section16/screens/tasks_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TasksScreen(),
+    );
+  }
+}
