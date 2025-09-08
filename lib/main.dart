5f9ce6ca0a0e5de565c52fef0af2f424f06d1f6c
Bill Sanderson <bill@towniebagels.com>
2025-09-08 10:45:59 -0700

    finished   section16  198

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:section16/screens/tasks_screen.dart';
-//test4
+//finished 198
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -6,14 +6,41 @@ class AddTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+
+      padding: EdgeInsets.all(30.0),
       child: Column(
+
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Text('Add Task',
+          textAlign: TextAlign.center,
       style: TextStyle(
         fontSize: 30.0,
         color: Colors.lightBlueAccent,
       ),),
-          TextField(),
+          TextField(
+            textAlign: TextAlign.center,
+            autofocus: true,
+          ),
+          SizedBox(height: 20),
+          TextButton(
+            onPressed: () {
+              print("Pressed");
+            },
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.lightBlueAccent,       // button background
+              foregroundColor: Colors.white,
+              padding: EdgeInsets.symmetric(
+                horizontal: 32,  // more left/right space
+                vertical: 16,    // more top/bottom space
+              ),
+              //minimumSize: Size(250, 50),         // ensures button has a minimum size
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12), // rounded corners
+              ),// text & icon color
+            ),
+            child: Text("Click Me"),
+          )
 
         ],
       ),
