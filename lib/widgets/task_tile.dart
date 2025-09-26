c1583ba13886dc6bb6a1b9be0f51f956cdce35f5
Bill Sanderson <bill@towniebagels.com>
2025-09-26 09:55:07 -0700

    start  section16  200 mid point got check box working

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -5,7 +5,30 @@ class TaskTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('this is a task'),
-      trailing: Checkbox(value: false, onChanged: null,),
+      trailing: TaskCheckbox(),
+    );
+  }
+}
+
+class TaskCheckbox extends StatefulWidget {
+
+  @override
+  State<TaskCheckbox> createState() => _TaskCheckboxState();
+}
+
+class _TaskCheckboxState extends State<TaskCheckbox> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: isChecked,
+      onChanged: (newValue) {
+        setState(() {
+          isChecked = newValue!;
+        },);
+
+      }
     );
   }
 }
