fcbe17bf312d8a8f0929cb27818c7fb95ac87707
Bill Sanderson <bill@towniebagels.com>
2025-09-27 09:53:39 -0700

    start  section16  200 midlast part error line 20 to fix now?

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,34 +1,42 @@
 import 'package:flutter/material.dart';
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
 
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false ;
+  void checkboxCallback (bool checkboxState){
+    setState(() {
+      isChecked = checkboxState;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('this is a task'),
-      trailing: TaskCheckbox(),
+      title: Text('this is a task',
+      style: TextStyle(decoration: isChecked ? TextDecoration.lineThrough: null),),
+      trailing: TaskCheckbox(isChecked,checkboxCallback),
+      //trailing: TaskCheckbox(
+      //         checkboxState: isChecked,
+      //         onChanged: checkboxCallback, /
     );
   }
 }
 
-class TaskCheckbox extends StatefulWidget {
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  //final Function toggleCheckboxState;  not work needed set valuechanged bool?
+  final ValueChanged<bool?> toggleCheckboxState;
+  TaskCheckbox(this.checkboxState, this.toggleCheckboxState);
 
-  @override
-  State<TaskCheckbox> createState() => _TaskCheckboxState();
-}
-
-class _TaskCheckboxState extends State<TaskCheckbox> {
-  bool isChecked = false;
   @override
   Widget build(BuildContext context) {
     return Checkbox(
       activeColor: Colors.lightBlueAccent,
-      value: isChecked,
-      onChanged: (newValue) {
-        setState(() {
-          isChecked = newValue!;
-        },);
-
-      }
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
